5548880ae01c2fe6108656bfa9bc5b43a4202ed2
ihsanozlu <ihsanozlu@gmail.com>
2026-03-16 18:35:04 +0300

    replace sitemap integration with static sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
   site: 'https://wiplabs.app',
-  integrations: [sitemap()],
 });
